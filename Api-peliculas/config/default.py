dc200da7b5b638b4d0de110af993ed932fa6c0cf
Jeronimo Gonzalez <88728646+JeronimoGg@users.noreply.github.com>
2023-11-09 20:41:08 -0500

    Update default.py

--- Api-peliculas/config/default.py
+++ Api-peliculas/config/default.py
@@ -3,7 +3,7 @@ SECRET_KEY = '123447a47f563e90fe2db0f56b1b17be62378e31b7cfd3adc776c59ca4c75e2fc5
 PROPAGATE_EXCEPTIONS = True
 # Database configuration
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://Lorena:contraseña@localhost/nombre_de_la_base_de_datos'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:jeronimog123@localhost:5432/api-peliculas'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SHOW_SQLALCHEMY_LOG_MESSAGES = False
 
